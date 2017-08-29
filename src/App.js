c6535725a0015f4fb26bc3b8bac75e2dc3ff6fb5
derek-cook <dcook693@gmail.com>
2017-08-29 15:42:56 -0700

    set new theme and changed dropdown for adding data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Components/Navbar';
+import Navbar from './Components/Navbar';
 
 class App extends Component {
   render() {
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-className Navbar extends Component {
+class Navbar extends Component {
 
   render() {
     return (
@@ -24,7 +24,7 @@ className Navbar extends Component {
               <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
               <li><a href="#">Link</a></li>
               <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-plus"></span><span className="caret"></span></a>
                 <ul className="dropdown-menu">
                   <li><a href="#">Action</a></li>
                   <li><a href="#">Another action</a></li>
@@ -38,27 +38,17 @@ className Navbar extends Component {
             </ul>
             <form className="navbar-form navbar-left">
               <div className="form-group">
-                <input type="text" className="form-control" placeholder="Search">
+                <input type="text" className="form-control" placeholder="Search"/>
               </div>
               <button type="submit" className="btn btn-default">Submit</button>
             </form>
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">Link</a></li>
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
-              </li>
+              <li><a href="#">Login</a></li>
             </ul>
           </div>{/*<!-- /.navbar-collapse -->*/}
         </div>{/*<!-- /.container-fluid -->*/}
       </nav>
-);
+    );
   }
 }
 
