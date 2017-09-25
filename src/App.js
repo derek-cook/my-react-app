df7542fdd7dd8d657a8949822f7b1765e5abaaa7
derekiancook <dcook693@hotmail.com>
2017-09-24 21:21:33 -0700

    added sidebar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './Layout';
-
+import Sidebar from './Components/Sidebar';
 
 class App extends Component {
 
@@ -10,14 +10,16 @@ class App extends Component {
 
 
   render() {
-
     
-
+    
+    
     return (
       <div className="App">
         <div className="app-router-container">
-          <Route path="/" component={Layout}>
-          </Route>
+        <Route path="/" component={Sidebar}></Route>
+        <div className="main-panel">
+          <Route path="/" component={Layout}></Route>
+        </div>
         </div>
       </div>
     );
