2ca75185655a3c4a6babe1e64bfe8968b865ea1c
derek-cook <dcook693@gmail.com>
2017-10-13 03:18:31 -0700

    starting to implement controller functions

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import EmployeeController from '../../controllers/EmployeeController';
 
 var router = express.Router();
 
-router.get('/:resource', function(req, res, next) {
+router.get('/:resource', (req, res, next) => {
     var resource = req.params.resource;
 
+    if (resource == 'employee') {
+        
+    }
     res.json({
         confirmation: 'SUCCESS',
         resource: resource
